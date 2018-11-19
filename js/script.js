62749a6304443bf33e6a5868da1325367d16136b
Rukaiyaa Arssath <sarssath6520.student@hsfi.us>
2018-11-19 23:14:08 +0000

    IF Else

--- js/script.js
+++ js/script.js
@@ -4,10 +4,27 @@
 
 //GLOBAL VARIABLES
 /* global $ */
-$("button").click(function() {
-    $("#userChoice").text($("#input").val());
-});
+var computerChoice;
+var winner;
+var randomNumber = Math.random();
 
+$("#shoot").click(function(){
+    var userChoice  = $("#userChoice").val(); 
+    $("#userChoice").text(userChoice);
+    if (randomNumber > 0 && randomNumber <.33) {
+        computerChoice = "rock";
+    $("#computerChoice").text(computerChoice);
+    }
+    else if (randomNumber > .33 && randomNumber <.66) {
+        computerChoice = "paper";
+    $("#computerChoice").text(computerChoice);
+    } 
+    else if (randomNumber > .66 && randomNumber <1) {
+        computerChoice = "scissors";
+    $("#computerChoice").text(computerChoice);
+    }
+    $("#computerChoice").text(computerChoice);
+});
 
 // DOCUMENT READY FUNCTION BELOW
 
